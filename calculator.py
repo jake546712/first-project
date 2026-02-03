a8bc1c64a81b74f9e78ed05ecff375a94ae44e22
Ngoc Anh Huy Tran <ahuy27042007@gmail.com>
2026-02-02 22:58:15 -0600

    simple cal

--- calculator.py
+++ calculator.py
@@ -1,4 +1,26 @@
-print("Simple Calculator")
-num1 = float(input("Enter your first number: "))
-num2 = float(input("Enter your second number: "))
-int("Sum:", num1 + num2)
+print("SIMPLE CALCULATOR!!!!")
+line = input("Enter the calculation (example: 7 * 9, 8 + 2,...):").strip()
+part = line.split()
+if len(part) == 3:
+    try:
+        num1= float(part[0])
+        oper = (part[1])
+        num2 = float(part[2])
+
+        if oper == "x":
+            print("Answer:", num1*num2)
+        elif oper == "+":
+            print("Answer:", num1+num2)
+        elif oper == "-":
+            print("Answer:", num1-num2)
+        elif oper == ":":
+            if num2 == 0:
+                print("Error!!")
+            else:
+                print("Answer:", num1/num2)
+        else:
+            print("only + , - , x , :")
+    except:
+        print("Error")
+else:
+    print("Right numbers")
